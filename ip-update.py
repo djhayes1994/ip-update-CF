f053d3b22f9b3e4e82be70d63f49aa808d0eaaba
Daniel Hayes <djhayes1994@gmail.com>
2025-03-12 19:47:51 -0400

    chore(app): Redo logic to make it more readable and move var loading to helper. Also linted with blue.

--- ip-update.py
+++ ip-update.py
@@ -1,45 +1,39 @@
-import os
-
 from utilities.cf import CloudFlareUtil as cf
 from utilities.logger import Logger
-from utilities.helper import get_ip
+from utilities.helper import get_ip, var_loader
 
-from dotenv import load_dotenv
 
 log = Logger()
-load_dotenv()
 
-api_key = os.getenv("CLOUDFLARE_API_KEY", None)
-cloudflare_account = os.getenv("CLOUDFLARE_ACCOUNT_ID", None)
-zone_id = os.getenv("CLOUDFLARE_ZONE_ID", None)
-dns_record_id = os.getenv("CLOUDFLARE_DNS_RECORD_ID", None)
-a_record_name = os.getenv("CLOUDFLARE_A_RECORD_NAME", None)
-
-log.info(f"Loaded environment variables from .env file.")
-data = {
-    "api_key": api_key,
-    "cf_account": cloudflare_account,
-    "zone_id": zone_id,
-    "dns_record_id": dns_record_id,
-    "a_record_name": a_record_name
-}
+data = var_loader()
 
 cf_client = cf(data)
 
-if cf_client.no_empty_data_values():
-    current_ip, ip_status = get_ip()
-    if ip_status:
-        valid_token = cf_client.verify_token()
+log.info(f'Successfully initialized Cloud Flare client.')
 
-        if valid_token:
+current_ip, ip_status = get_ip()
 
-            update_result = cf_client.update_zone_record(ip=current_ip)
+if not cf_client.no_empty_data_values():
+    log.failure(
+        f'A value was empty in the .env file, please fill in all values.'
+    )
+    exit()
 
-            if update_result:
-                log.info(f"Successfully updated zone record ({a_record_name}) with value of: {current_ip}")
-            else:
-                log.failure(f"Failed to update zone record ({a_record_name}) with value of: {current_ip}")
-    else:
-        log.failure("Exiting script due to failure grabbing IP.")
-else:
-    log.failure(f"A value was empty in the .env file, please fill in all values.")
+if not ip_status:
+    exit()
+
+if not cf_client.verify_token():
+    log.failure(
+        f'Failed to verify CloudFlare token, token is either invalid or there was an error in the request.'
+    )
+    exit()
+
+if not cf_client.update_zone_record(ip=current_ip):
+    log.failure(
+        f"Failed to update zone record ({data['a_record_name']}) with value of: {current_ip}"
+    )
+    exit()
+
+log.info(
+    f"Successfully updated zone record ({data['a_record_name']}) with value of: {current_ip}"
+)
